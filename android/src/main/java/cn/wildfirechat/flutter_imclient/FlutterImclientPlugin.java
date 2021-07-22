4bc68c81ad1795238aa5fc9147bd34c270577608
WillZH <willzh@live.cn>
2021-07-22 11:08:06 +0800

    change APPLICATION_ID to LIBRARY_PACKAGE_NAME

--- android/src/main/java/cn/wildfirechat/flutter_imclient/FlutterImclientPlugin.java
+++ android/src/main/java/cn/wildfirechat/flutter_imclient/FlutterImclientPlugin.java
@@ -118,7 +118,7 @@ public class FlutterImclientPlugin implements FlutterPlugin, MethodCallHandler, 
     channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_imclient");
     channel.setMethodCallHandler(this);
 
-    PushService.init(flutterPluginBinding.getApplicationContext(), BuildConfig.APPLICATION_ID, new PushService.IPushCallback() {
+    PushService.init(flutterPluginBinding.getApplicationContext(), BuildConfig.LIBRARY_PACKAGE_NAME, new PushService.IPushCallback() {
       @Override
       public void onPushToken(int pushType, String pushToken) {
         if(TextUtils.isEmpty(pushToken)) {
